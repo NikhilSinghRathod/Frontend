e2ad9019b1696bfbc5ca2fcd3bd7ab9d78bad54f
NikhilSinghRathod <r7445580@gmail.com>
2025-03-04 13:22:35 +0530

    [Nikhil Singh Rathode] Adding . UC1 Validate Email address with a regex. The email consists of minimum 3 and optional 2 more parts with mandatory @ and .abc.xyz@bridgelabz.co.in

---
+++ pattern/email/uc-1.js
@@ -0,0 +1,13 @@
+function isValidEmail(email){
+    const regex = /^abc(\.[a-z]+)?@bridgelabz.co(\.[a-z]+)?$/;
+    if(regex.test(email)){
+        return "Email Validated";
+    }
+    else {
+        return "Invalid Email";
+    }
+}
+console.log(isValidEmail("abc@yahoo.com"));
+console.log(isValidEmail("abc-100@yahoo.com"));
+console.log(isValidEmail("abc.india@bridgelabz.co.in"));
+
